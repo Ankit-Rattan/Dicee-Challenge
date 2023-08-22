c65cf6642c2425f43ad3d520f13e3da1c2d9448f
Ankit Rattan <rattanankit2004@gmail.com>
2023-08-22 17:31:24 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var randomNumber1 = Math.floor(Math.random() * 6) +1;
+var randomNumber2 = Math.floor(Math.random() * 6) +1;
+
+console.log(randomNumber1);
+console.log(randomNumber2);
+
+var setone = document.querySelectorAll("img")[0]
+setone.setAttribute("src", "./images/dice" + randomNumber1 + ".png")
+
+
+
+var settwo =document.querySelectorAll("img")[1]
+settwo.setAttribute("src", "images/dice" + randomNumber2 + ".png")
+
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins !"
+}   
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins !"
+    
+}
+else{
+    document.querySelector("h1").innerHTML = "! Draw !"
+    
+}
+
+
+
